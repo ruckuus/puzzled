b3d32e2acd698165ce78eac8f3800c4efa042bee
Dwi Sasongko S <ruckuus@gmail.com>
2014-03-14 15:41:52 +0800

    Descending

--- shell/cerewet.sh
+++ shell/cerewet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo cat /var/log/nginx/web*/*/access.log | awk '{ print $7 }' | sed 's/"//g' | sed 's/,//g' | grep -v "-" | sort -n | uniq -c | sort -n | tail -n 30
+sudo cat /var/log/nginx/web*/*/access.log | awk '{ print $7 }' | sed 's/"//g' | sed 's/,//g' | grep -v "-" | sort -n | uniq -c | sort -n -r | head -n 30
