d8b3b15b1e750320703ba333e1092bd3f1565197
Dwi Sasongko Supriyadi <dwi@treeboxsolutions.com>
2015-05-14 00:00:58 +0800

    Selection sort.

---
+++ puzzle/sel_sort.js
@@ -0,0 +1,34 @@
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function(array, startIndex) {
+
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for(var i = minIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    } 
+    return minIndex;
+}; 
+
+var selectionSort = function(array) {
+    var curr = 0;
+    
+    for(curr = 0; curr < array.length; curr++) {
+        var currVal = indexOfMinimum(array, curr);
+        swap(array, curr, currVal);
+    }
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+selectionSort(array);
+println("Array after sorting:  " + array);
+
+Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
